a06da4735f60c85ff653739236812adb3840f3ff
Michał Reguła <w66080@student.wsiz.edu.pl>
2022-06-12 10:28:05 +0200

    Init commit

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+//zadanie 4
+int main()
+{
+
+	char s1[] = "wiosna";
+	char s2[] = "'a','b','c','\0'";
+	char s3[20];
+
+	cout << "Podaj imie i nazwisko ";
+	cin.getline(s3, 20).get();
+	cout << s3;
+	strcpu_s(s1, s2);
+	strcat_s(s1, s2);
+	
+	size(s1);
+	sizeof(s1) / sizeof(s1[0]); _
+}
